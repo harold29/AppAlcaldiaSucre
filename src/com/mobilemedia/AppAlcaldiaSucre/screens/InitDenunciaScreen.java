627fd6715588db3be1c127b59ad3fc6d288b4a45
Harold29 <haroldjose2903@gmail.com>
2013-10-01 12:33:15 -0430

    Push agregado, ya con esto hasta el momento se cree que se puede hacer los envíos con estas librerías. Hace falta igualmente el arreglo de la interfaz general, la documentacion de las clases nuevas y además la adaptación a los distintos tipos de pantallas. Hay que terminar la clase InitDenunciaScreen y ademas probar el envío de datos a un servidor cualquiera en forma de texto plano.

--- src/com/mobilemedia/AppAlcaldiaSucre/screens/InitDenunciaScreen.java
+++ src/com/mobilemedia/AppAlcaldiaSucre/screens/InitDenunciaScreen.java
@@ -10,6 +10,10 @@ import net.rim.device.api.ui.container.MainScreen;
 import com.mobilemedia.AppAlcaldiaSucre.componentes.ListStyleButtonField;
 import com.mobilemedia.AppAlcaldiaSucre.componentes.ListStyleButtonSet;
 import com.mobilemedia.AppAlcaldiaSucre.custom.Constantes;
+import com.mobilemedia.AppAlcaldiaSucre.engine.ScreenEngine;
+import com.mobilemedia.AppAlcaldiaSucre.push.PersistentStorage;
+import com.mobilemedia.AppAlcaldiaSucre.push.PushController;
+import com.mobilemedia.AppAlcaldiaSucre.push.PushMessage;
 
 
 public class InitDenunciaScreen extends MainScreen implements FieldChangeListener {
@@ -22,11 +26,27 @@ public class InitDenunciaScreen extends MainScreen implements FieldChangeListene
 		return instance;
 	}
 	
+	private static final int SEP_IDX1 = 0x00010000;
+    private static final int SEP_IDX2 = 0x00020000;
+	
 	private ListStyleButtonField listStyleButtonFieldDenuncias;
-	private ListStyleButtonField listStyleButtonFIeldRegistro;
+	private ListStyleButtonField listStyleButtonFieldRegistro;
+	private VerticalFieldManager pantalla;
 	
 	public InitDenunciaScreen () {
+		pantalla = new VerticalFieldManager(Field.NON_FOCUSABLE | USE_ALL_WIDTH);
+		pantalla.setBackground(BackgroundFactory.createSolidBackground(0x00FCB53E));
 		
+		listStyleButtonFieldDenuncias = new ListStyleButtonField("Hacer Denuncia", Constantes.MEDIUM_NORMAL_FONT, FIELD_HCENTER | FIELD_VCENTER);
+		listStyleButtonFieldDenuncias.setChangeListener(this);
+		
+		listStyleButtonFieldRegistro = new ListStyleButtonField("Registrar", Constantes.MEDIUM_NORMAL_FONT, FIELD_HCENTER | FIELD_VCENTER);
+		listStyleButtonFieldRegistro.setChangeListener(this);
+		
+		pantalla.add(listStyleButtonFieldDenuncias);
+		if (!PersistentStorage.isRegistered()) {
+			pantalla.add(listStyleButtonFieldRegistro);
+		}
 	}
 	
 	/*private static class RegisterMenuItem extends MenuItem {
@@ -41,8 +61,34 @@ public class InitDenunciaScreen extends MainScreen implements FieldChangeListene
 		}
 	}*/
 	
+	private static class RegisterMenuItem extends MenuItem {
+		public RegisterMenuItem() {
+			super (PersistentStorage.isRegistered() ? "Configurar registro" : "Registrar", SEP_IDX1 + 1, 1);
+		}
+
+		public void run() {
+			// TODO Auto-generated method stub
+			PushController.register();
+		}
+	}
+	
+	private static class EnviarDatos extends MenuItem {
+		
+		public EnviarDatos() {
+			super("Enviar Datos", SEP_IDX1 + 2, 1);
+		}
+		
+		public void run() {
+			PushMessage message = new PushMessage();
+			PushController.onMessage(message);
+		}
+	}
+	
 	public void fieldChanged(Field field, int context) {
-		if (field == listStyleButtonFieldDenuncias) {}
-		if (field == listStyleButtonFIeldRegistro) {}
+		if (field == listStyleButtonFieldDenuncias) 
+		{
+			ScreenEngine.getInstance().goDenuncias();
+		}
+		if (field == listStyleButtonFieldRegistro) {}
 	}
 }
